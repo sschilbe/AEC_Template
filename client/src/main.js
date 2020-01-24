1253a53da3370f298666ee22454612f72bf2302f
Mackenzie Toner <mackenzie.toner@unb.ca>
2020-01-24 09:43:58 -0400

    Cleanup vue project

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -9,9 +8,8 @@ Vue.config.productionTip = false
 
 new Vue({
  render: h => h(App),
- router,
  vuetify,
- /* 
+ /*
   * Put all globally available data here, this data can be accessed in .Vue files via:
   * -this.$root.$data...-
   */
