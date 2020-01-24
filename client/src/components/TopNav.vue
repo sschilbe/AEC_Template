1ec4c26af095f5df9c0f8f70717f24f9538ffc8b
shivamsabbarwal <shivam.sabb@gmail.com>
2020-01-24 11:49:20 -0400

    Fix the styling on the top nav

--- client/src/components/TopNav.vue
+++ client/src/components/TopNav.vue
@@ -1,22 +1,17 @@
 <template>
-  <v-card class="overflow-hidden">
     <v-app-bar absolute color="purple" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
+      <v-spacer></v-spacer>
       <v-toolbar-title>AEC 2020: Programming Competition</v-toolbar-title>
+      <v-spacer></v-spacer>
 
       <template v-slot:extension>
-        <v-tabs align-with-title background-color="transparent">
+        <v-tabs align-with-title grow="grow" background-color="transparent">
           <v-tab>Saint John, NB</v-tab>
           <v-tab>Halifax, NS</v-tab>
           <v-tab>St. John's, NF</v-tab>
         </v-tabs>
       </template>
     </v-app-bar>
-    <v-sheet>
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
-  </v-card>
 </template>
 
 <script>
