0b826e2a2750cbe87d2513d359e3706ba511e5ce
shivamsabbarwal <shivam.sabb@gmail.com>
2020-01-24 10:23:37 -0400

    Added Nav at the top to switch between cities

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="purple"
-      dark
-    >
-      <div class="d-flex align-center">
-        <p class="text-center">AEC 2020</p>
-      </div>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <TopNav/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TopNav from './components/TopNav';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TopNav,
   },
 
   data: () => ({
---
+++ client/src/components/TopNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar absolute color="purple" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>AEC 2020: Programming Competition</v-toolbar-title>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title background-color="transparent">
+          <v-tab>Saint John, NB</v-tab>
+          <v-tab>Halifax, NS</v-tab>
+          <v-tab>St. John's, NF</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet>
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TopNav",
+
+  data() {
+    return {};
+  }
+};
+</script>
