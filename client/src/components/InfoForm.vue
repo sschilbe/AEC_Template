34b6dff95fd90c95c1a8f70f5632749f540291f1
shivamsabbarwal <shivam.sabb@gmail.com>
2020-01-24 17:24:55 -0400

    input did not work

--- client/src/components/InfoForm.vue
+++ client/src/components/InfoForm.vue
@@ -1,23 +1,23 @@
 <template>
   <v-form class="form">
     <v-container>
-      <v-row>
+      <!-- <v-row>
         <v-col cols="8" sm="4">
-          <v-text-field color="purple" :v-model="budget" label="Budget" outlined></v-text-field>
+          <input color="purple" :v-model="budget" label="Budget" outlined/>
         </v-col>
 
         <v-col cols="8" sm="4">
-          <v-text-field
+          <input
             color="purple"
             :v-model="capture"
             label="Target Carbon Capture Percentage"
             outlined
-          ></v-text-field>
+          />
         </v-col>
         <v-col cols="8" sm="4">
           <v-btn class="mr-4" @click="submitForm()">submit</v-btn>
         </v-col>
-      </v-row>
+      </v-row> -->
 
       <v-row>
         <v-col cols="12" sm="6">
@@ -57,4 +57,52 @@ export default {
 .form {
   margin-top: 110px;
 }
+
+input {
+	outline: none;
+    padding: 10px;
+	border: 2px solid #9c27b0;
+    border-radius: 15px;
+}
+
+textarea {
+  outline: none;
+  border: none;
+}
+
+textarea:focus, input:focus {
+  border-color: #d6d6d6;
+}
+
+input:focus::-webkit-input-placeholder { color:transparent; }
+input:focus:-moz-placeholder { color:transparent; }
+input:focus::-moz-placeholder { color:transparent; }
+input:focus:-ms-input-placeholder { color:transparent; }
+
+textarea:focus::-webkit-input-placeholder { color:transparent; }
+textarea:focus:-moz-placeholder { color:transparent; }
+textarea:focus::-moz-placeholder { color:transparent; }
+textarea:focus:-ms-input-placeholder { color:transparent; }
+
+input::-webkit-input-placeholder { color: #acacac; }
+input:-moz-placeholder { color: #acacac; }
+input::-moz-placeholder { color: #acacac; }
+input:-ms-input-placeholder { color: #acacac; }
+
+textarea::-webkit-input-placeholder { color: #acacac; }
+textarea:-moz-placeholder { color: #acacac; }
+textarea::-moz-placeholder { color: #acacac; }
+textarea:-ms-input-placeholder { color: #acacac; }
+
+/*---------------------------------------------*/
+button {
+	outline: none !important;
+	border: none;
+	background: transparent;
+}
+
+button:hover {
+	cursor: pointer;
+}
+
 </style>
