685b4654df2a8372f7b5b0fb609d54d7ad41da05
sschilbe <seth.schilbe@gmail.com>
2020-01-24 09:32:46 -0400

    Add city grid endpoint

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
-from database import Database
+import json
 import os
 
 # configuration
@@ -23,6 +23,35 @@ def ping_pong():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/cityGrid')
+def cityGrid():
+    city = request.args.get('city')
+    budget = 1750000
+    carbonCapturePercentage = 21
+    grid, totalSpent, actualCarbonCapturePercent = getFilledGrid( city, budget, carbonCapturePercentage )
+    data = json.dumps({
+        'city': city,
+        'dimensions': {
+            'length': len( grid ),
+            'width': len( grid[0] )
+        },
+        'grid': grid,
+        'results': {
+            'budget': budget,
+            'totalSpent': totalSpent,
+            'targetCarbonCapture': carbonCapturePercentage,
+            'actualCarbonCapture': actualCarbonCapturePercent
+        }
+    })
+
+    return data
+
+def getFilledGrid( city, budget, carbonCapturePercentage ):
+    grid = [[]]
+    totalSpent = 1500000
+    actualCarbonCapturePercent = 22
+    return grid, totalSpent, actualCarbonCapturePercent
 
 if __name__ == '__main__':
     app.run()
