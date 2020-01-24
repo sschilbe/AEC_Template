e9401e7819a3d701ce70ec906a8612b90b725bea
dmccardle <danny.mccardle@unb.ca>
2020-01-24 10:51:57 -0400

    implemented methods to parse in city data from text files

--- server/app.py
+++ server/app.py
@@ -13,16 +13,66 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+# classes
+class City():
+    def __init__(self, name, num_rows, num_cols, grid_data):
+        self.name = name
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.grid_data = grid_data
 
-# Favicon to display in
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+list_of_cities = []
+
+# parses in city data from text files and adds to list_of_cities
+@app.route('/parse')
+def parse_city_data():
+    halifax_data_file = open('./data/cities/Halifax.txt', 'r')
+    halifax_data_lines = halifax_data_file.readlines()
+    halifax = parse_city_from_data_lines(halifax_data_file)
+    halifax_data_file.close()
+
+    saintjohn_data_file = open('./data/cities/SaintJohn.txt', 'r')
+    saintjohn_data_lines = saintjohn_data_file.readlines()
+    saintjohn = parse_city_from_data_lines(saintjohn_data_lines)
+    saintjohn_data_file.close()
+    
+    stjohns_data_file = open('./data/cities/SaintJohn.txt', 'r')
+    stjohns_data_lines = stjohns_data_file.readlines()
+    stjohns = parse_city_from_data_lines(stjohns_data_lines)
+    stjohns_data_file.close()
+
+    list_of_cities.append(halifax)
+    list_of_cities.append(saintjohn)
+    list_of_cities.append(stjohns)
+
+# builds and returns a City object based on the list of data provided
+def parse_city_from_data_lines(city_data_lines):
+    name = city_data_lines[0]
+    num_rows = city_data_lines[2]
+    num_cols = city_data_lines[3]
+    grid_data = build_grid_data(num_rows, num_cols, city_data_lines)
+    city = City(name, num_rows, num_cols, grid_data)
+    return city
+
+# builds and returns a 2-D list representing the grid of data for the city
+def build_grid_data(num_rows, num_cols, city_data_lines):
+    grid_data = []
+    grid_id = 1
+    for i in range(4, num_rows):
+        grid_data_line = city_data_lines[i].split(',')
+        new_li = []
+        for j in range(0, num_cols, 2):
+            value = grid_data_line[j]
+            acceptable_location = grid_data_line[j+1]
+            new_dict = {}
+            new_dict["id"] = grid_id
+            new_dict["value"] = value
+            new_dict["acceptable_location"] = acceptable_location
+            new_li.append(new_dict)
+            grid_id += 1 # increment to next id
+        grid_data.append(new_li)
+    return grid_data
+    
 
 @app.route('/cityGrid')
 def cityGrid():
